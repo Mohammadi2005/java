ca47d5c207000078fe117ef5fe275dda08115be8
amir hossein <amohammadi161183@gmail.com>
2025-06-08 21:28:15 +0330

    Add MyNewFile.java related to commit f84db6f612bd35b20fb2373bab8b66582d1de7a8

---
+++ InterFace/zoo.java
@@ -0,0 +1,9 @@
+package InterFace;
+
+public class zoo {
+    public static void main(String[] arg){
+        Chiken chi = new Chiken("morq", 2, 2, 0);
+        System.out.println(chi.getAge());
+
+    }
+}
